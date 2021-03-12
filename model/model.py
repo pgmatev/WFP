7fc6f05603692c63af6cffd7f9ebefcecc012405
joro2404 <joro2404@abv.bg>
2021-03-13 00:10:50 +0200

    model testing, pipeline not working

--- model/model.py
+++ model/model.py
@@ -1,24 +1,26 @@
 import numpy as np
 import keras
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential #For Initializing ANN
-from tensorflow.keras.layers import Dense #For Layers of ANN
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense 
 from data import X_train, y_train
-# Initializing the ANN with sequence of layers (Could use a Graph)
-#Classifier Model
 
 
-model = Sequential()
-# Adding the input layer and the first hidden layer
-model.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu', input_dim = 27))
-# Adding the hidden layers
-model.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu'))
-model.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu'))
-# Adding the output layer
-# Probability for the outcome 
-model.add(Dense(units = 7, kernel_initializer = 'uniform', activation = 'softmax'))
-# Compiling the ANN
-'''Classification'''
-#Another Option: categorical_crossentropy
+def create_model():
+    model = Sequential()
+
+    model.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu', input_dim = 27))
+
+    model.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu'))
+    model.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu'))
+
+    model.add(Dense(units = 7, kernel_initializer = 'uniform', activation = 'softmax'))
+
+    return model
+
+
+model = create_model()
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 model.fit(X_train, y_train, batch_size = 5, epochs = 500)
+
+model.save('model.hdf5')
