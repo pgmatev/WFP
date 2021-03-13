262ae28b113d90ded9a2644bb74eb304a4d9108d
veselin-angelov <veselin.angelov02@gmail.com>
2021-03-13 17:10:30 +0200

    added hexagon building

--- flask_app/app/main.py
+++ flask_app/app/main.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, Blueprint, jsonify
 from flask_googlemaps import Map
+import requests
+from os import environ
 
 
 main = Blueprint('main', __name__)
@@ -24,9 +26,22 @@ def index():
 def get_info():
     req = request.get_json(force=True)
 
-    print(req)
+    latitude = req.get('latitude')
+    longitude = req.get('longitude')
+
+    get_weather_info(latitude, longitude)
 
     return jsonify({'resp': 'asd'})
+
+
+def get_weather_info(lat, lng):
+    api_key = environ.get('WEATHER_API_KEY')
+    weather_api_url = f'https://api.weatherbit.io/v2.0/current?lat={lat}&lon={lng}&key={api_key}'
+    weather_info = requests.get(weather_api_url)
+
+    print(weather_info.content.decode())
+
+    return 'ok'
 
 
 if __name__ == '__main__':
--- flask_app/app/static/js/hexagon.js
+++ flask_app/app/static/js/hexagon.js
@@ -17,27 +17,17 @@ function drawHexagon(map,position,radius){
         fillOpacity: 0.35
     });
     polygon.setMap(map);
-    // map.setCenter(position);
 }
 
 function drawHexagon1(map,position,radius){
-    var coordinates = [];
-    for(var angle= -60;angle < 300; angle+=60) {
-        let cor = google.maps.geometry.spherical.computeOffset(position, radius, angle)
-        coordinates.push(cor);
-        let br = radius / Math.sqrt(3)
-        drawHexagon(gmap, cor, br)
+    let br = radius / Math.sqrt(3)
+    for (var i = 0; i < 3; i++) {
+        position = google.maps.geometry.spherical.computeOffset(position, radius, 0);
+        for (var j = 1; j <= 6; j++) {
+            for (var k = 1; k <= i+1; k++) {
+                drawHexagon(gmap, position, br);
+                position = google.maps.geometry.spherical.computeOffset(position, radius, -60+(-60*j));
+            }
+        }
     }
-
-    // console.log(coordinates);
-
-    // Construct the polygon.
-    var polygon = new google.maps.Polygon({
-        paths: coordinates,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35
-    });
 }
